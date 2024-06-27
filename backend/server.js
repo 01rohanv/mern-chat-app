29f3e9d08580ecde74ea5ed2d389a362baa4ba9d
Rohan Verma <rv0181998@gmail.com>
2024-06-27 17:50:23 +0530

    db folder changed clg

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,18 @@ import userRoutes from "../backend/routes/user.routes.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
 import path from "path";
-import connectToMongoDb from "./db/connectToMongoDb.js";
+import mongoose from "mongoose";
 
 const PORT = process.env.PORT || 5000;
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB", error.message);
+  }
+};
 
 const __dirname = path.resolve();
 
@@ -32,6 +41,6 @@ app.get("*", (req, res) => {
 // });
 
 server.listen(PORT, () => {
-  connectToMongoDb();
+  connectToMongoDB();
   console.log(`Server is running on Port ${PORT}`);
 });
