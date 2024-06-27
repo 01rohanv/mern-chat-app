f9ebabd8af4f253fb9feb03c5621f0ea5bba9d4e
Rohan Verma <rv0181998@gmail.com>
2024-06-27 15:38:25 +0530

    deployement

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,11 @@ import userRoutes from "../backend/routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDb.js";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/socket.js";
+import path from "path";
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -16,6 +19,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // app.get("/", (req, res) => {
 //   res.send("Hello checking");
