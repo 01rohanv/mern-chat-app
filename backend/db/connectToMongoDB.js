108d25e4e885601d2475e8fed19c004a49129182
Rohan Verma <rv0181998@gmail.com>
2024-06-27 17:21:48 +0530

    db folder changed clg

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const connectToMongoDb = async () => {
   try {
     await mongoose.connect(process.env.MONGO_DB_URI);
-    console.log("Connected to MongoDb");
   } catch (error) {
     console.log('"Error connecting', error.message);
   }
